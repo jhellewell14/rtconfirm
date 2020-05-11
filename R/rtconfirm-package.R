989f3e815723b82018c124d984c29a4ba50fb687
Joel Hellewell <j.hellewell14@imperial.ac.uk>
2020-05-11 19:16:46 +0100

    Initial commit

---
+++ R/rtconfirm-package.R
@@ -0,0 +1,16 @@
+#' The 'rtconfirm' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name rtconfirm-package
+#' @aliases rtconfirm
+#' @useDynLib rtconfirm, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#'
+NULL
